2dc862ebc9f6547e94d3cc209f09eaf49e4dce41
vinodhwrites@gmail.com <vinodhwrites@gmail.com>
2025-09-17 17:35:00 +0530

    latest

--- src/main/java/com/restapi/controller/ComputerAPI.java
+++ src/main/java/com/restapi/controller/ComputerAPI.java
@@ -3,12 +3,17 @@ package com.restapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restapi.entities.Computer;
 import com.restapi.service.ComputerService;
+import com.restapi.util.RecordAlreadyExistsException;
 
 @RestController
 @RequestMapping("/api/computers")
@@ -19,5 +24,21 @@ public class ComputerAPI {
 	public List<Computer> getAll(){
 		return cservice.getComputers();
 	}
+	
+	@PostMapping("")
+	public ResponseEntity<Object> addComputer(@RequestBody Computer computer)
+	{
+       try{
+          cservice.addComputer(computer);
+          return new ResponseEntity<>(computer,HttpStatus.CREATED);
+       }
+       catch(RecordAlreadyExistsException e) {
+    	   return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+       }
+       catch(Exception e) {
+    	   return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+       }
+       }
+	}
 
-}
+
